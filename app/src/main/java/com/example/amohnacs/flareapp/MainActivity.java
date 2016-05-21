54b49c94a742604bd0a584d67b6a3c3acbd17b4a
Sunny Raj <sundarcs@gwu.edu>
2016-05-21 15:14:36 -0400

    Display arcGIS map

--- app/src/main/java/com/example/amohnacs/flareapp/MainActivity.java
+++ app/src/main/java/com/example/amohnacs/flareapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.amohnacs.flareapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
     } //commit
 }
