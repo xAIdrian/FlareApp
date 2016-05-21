725b63b68afe5053a4b928c837c8c69038c00a25
Sunny Raj <sundarcs@gwu.edu>
2016-05-21 15:26:41 -0400

    Add references to the MapView

--- app/src/main/java/com/example/amohnacs/flareapp/MapActivity.java
+++ app/src/main/java/com/example/amohnacs/flareapp/MapActivity.java
@@ -1,10 +1,17 @@
 package com.example.amohnacs.flareapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.esri.android.map.GraphicsLayer;
+import com.esri.android.map.MapView;
+import com.esri.core.geometry.Point;
 
 public class MapActivity extends AppCompatActivity {
-
+    MapView mMapView;
+    GraphicsLayer mLocationLayer;
+    Point mLocationLayerPoint;
+    String mLocationLayerPointString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
