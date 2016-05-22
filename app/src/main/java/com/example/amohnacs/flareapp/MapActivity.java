1176e0d1659cb96f0978990b184f83a1f91afc83
Adrian Mohnacs <amohnacss@gmail.com>
2016-05-22 11:13:50 -0400

    Data wipe/reset and flare animation

--- app/src/main/java/com/example/amohnacs/flareapp/MapActivity.java
+++ app/src/main/java/com/example/amohnacs/flareapp/MapActivity.java
@@ -1,16 +1,32 @@
 package com.example.amohnacs.flareapp;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.Animatable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.esri.android.map.GraphicsLayer;
 import com.esri.android.map.LocationDisplayManager;
@@ -33,14 +49,29 @@ public class MapActivity extends AppCompatActivity implements FeatureCallback {
     private ArcGISFeatureLayer mFeatureLayer;
     private FeatureLayerClass mFeature;
 
+    private Handler handler;
+
     public MapView mMapView;
     public LocationDisplayManager ldm;
     public Location mLocation;
-    public ImageButton mImageButton;
+    public ImageView mImageButton;
 
     ActionBarDrawerToggle mActionToggle;
     DrawerLayout mDrawerLayout;
     NavigationView mNavigation;
+
+    LinearLayout mSelectionLayout;
+    TextView mSocialText;
+    TextView mEmergencyText;
+    TextView mServicesText;
+
+    TranslateAnimation slideDown;
+    TranslateAnimation slideUp;
+    private Animation popAnim;
+    private Animation popSettleAnim;
+    private Animation swipeAnimation;
+    private Animation closeAnim;
+    AlertDialog.Builder builderSingle;
 
     PictureMarkerSymbol mPictureMarkerSymbol;
 
@@ -56,6 +87,11 @@ public class MapActivity extends AppCompatActivity implements FeatureCallback {
 
 
         mPictureMarkerSymbol = new PictureMarkerSymbol(getResources().getDrawable(R.drawable.pin));
+        handler = new Handler();
+        popAnim = AnimationUtils.loadAnimation(this, R.anim.pop);
+        popSettleAnim = AnimationUtils.loadAnimation(this, R.anim.pop_settle);
+        closeAnim = AnimationUtils.loadAnimation(this, R.anim.close);
+
         mActionToggle = new ActionBarDrawerToggle(
                 this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         );
@@ -83,6 +119,124 @@ public class MapActivity extends AppCompatActivity implements FeatureCallback {
                 });
 
 
+         mSelectionLayout = (LinearLayout) findViewById(R.id.selectionLayout);
+         mSocialText = (TextView) findViewById(R.id.socialTextView);
+         mEmergencyText = (TextView) findViewById(R.id.emergencyTextView);
+         mServicesText = (TextView) findViewById(R.id.servicesTextView);
+
+
+        slideDown= new TranslateAnimation(0, 0, 0, 100);
+        slideDown.setDuration(300);
+        slideUp= new TranslateAnimation(0, 0, 100, 0);
+        slideUp.setDuration(300);
+        swipeAnimation = AnimationUtils.loadAnimation(this, R.anim.fling);
+
+        builderSingle = new AlertDialog.Builder(this);
+        builderSingle.setIcon(R.drawable.blue_logo);
+        builderSingle.setTitle("Please select a service");
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1);
+        arrayAdapter.add("Auto Towing");
+        arrayAdapter.add("Movers");
+        arrayAdapter.add("Home Cleaning");
+        arrayAdapter.add("Escort");
+
+        builderSingle.setNegativeButton(
+                "cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        mSelectionLayout.startAnimation(slideUp);
+                        mSelectionLayout.setVisibility(View.VISIBLE);
+                    }
+                });
+
+        builderSingle.setAdapter(
+                arrayAdapter,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String strName = arrayAdapter.getItem(which);
+
+                        dialog.dismiss();
+
+                        mImageButton.setImageResource(R.drawable.yellow_logo);
+                        mImageButton.setVisibility(View.VISIBLE);
+                        mImageButton.startAnimation(popAnim);
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mImageButton.startAnimation(popSettleAnim);
+                            }
+                        }, popAnim.getDuration());
+                    }
+                });
+
+        mSocialText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSelectionLayout.startAnimation(slideDown);
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSelectionLayout.setVisibility(View.INVISIBLE);
+
+                        mImageButton.setImageResource(R.drawable.blue_logo);
+                        mImageButton.setVisibility(View.VISIBLE);
+                        mImageButton.startAnimation(popAnim);
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mImageButton.startAnimation(popSettleAnim);
+                            }
+                        }, popAnim.getDuration());
+                    }
+                },slideDown.getDuration());
+            }
+        });
+        mEmergencyText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSelectionLayout.startAnimation(slideDown);
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSelectionLayout.setVisibility(View.INVISIBLE);
+
+                        mImageButton.setImageResource(R.drawable.red_logo);
+                        mImageButton.setVisibility(View.VISIBLE);
+                        mImageButton.startAnimation(popAnim);
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mImageButton.startAnimation(popSettleAnim);
+                            }
+                        }, popAnim.getDuration());
+                    }
+                },slideDown.getDuration());
+            }
+        });
+        mServicesText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSelectionLayout.startAnimation(slideDown);
+                builderSingle.show();
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSelectionLayout.setVisibility(View.INVISIBLE);
+                    }
+                },slideDown.getDuration());
+            }
+        });
+
+
         mMapView = (MapView) findViewById(R.id.map);
         // Create GraphicsLayer
         graphicsLayer = new GraphicsLayer();
@@ -105,6 +259,7 @@ public class MapActivity extends AppCompatActivity implements FeatureCallback {
 
                             mLocation.setLongitude(location.getLongitude());
                             mLocation.setLatitude(location.getLatitude());
+
 
                             if (location != null) {
                                 Log.d("SUNNY", "lat: " + location.getLatitude() + " long: " + location.getLongitude());
@@ -140,9 +295,22 @@ public class MapActivity extends AppCompatActivity implements FeatureCallback {
             }
         });
 
-        mImageButton = (ImageButton) findViewById(R.id.flareButton);
+        mImageButton = (ImageView) findViewById(R.id.flareButton);
         mImageButton.setOnTouchListener(new OnSwipeTouchListener(MapActivity.this) {
+
             public void onSwipeTop() {
+
+                mImageButton.startAnimation(swipeAnimation);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mImageButton.setVisibility(View.INVISIBLE);
+
+                        mSelectionLayout.setVisibility(View.VISIBLE);
+                        mSelectionLayout.startAnimation(slideUp);
+                    }
+                }, swipeAnimation.getDuration());
+
                 Point tempPoint = new Point(mLocation.getLongitude(), mLocation.getLatitude());
                 Point pointGeometry = (Point) GeometryEngine.project(tempPoint, SpatialReference.create(4326), SpatialReference.create(3857));
                 // create a graphic with the geometry and marker symbol
@@ -161,24 +329,29 @@ public class MapActivity extends AppCompatActivity implements FeatureCallback {
 
     private void fireFlares() {
         Location location = getNearbyRandomLocation(
-                mLocation.getLongitude(), mLocation.getLatitude(), 25);
+                mLocation.getLongitude(), mLocation.getLatitude(), 500);
         mFeature.submitFlare(
                 new Flare("Tyler Durdern", new LatLng(location.getLongitude(), location.getLatitude()), "Social"));
 
         Location location1 = getNearbyRandomLocation(
-                mLocation.getLongitude(), mLocation.getLatitude(), 25);
+                mLocation.getLongitude(), mLocation.getLatitude(), 500);
         mFeature.submitFlare(
                 new Flare("Wade Winston Wilson", new LatLng(location1.getLongitude(), location1.getLatitude()), "Service"));
 
         Location location2 = getNearbyRandomLocation(
-                mLocation.getLongitude(), mLocation.getLatitude(), 25);
+                mLocation.getLongitude(), mLocation.getLatitude(), 1000);
         mFeature.submitFlare(
                 new Flare("Dick Grayson", new LatLng(location2.getLongitude(), location2.getLatitude()), "Emergency"));
 
         Location location3 = getNearbyRandomLocation(
-                mLocation.getLongitude(), mLocation.getLatitude(), 25);
+                mLocation.getLongitude(), mLocation.getLatitude(), 1000);
         mFeature.submitFlare(
                 new Flare("James Howlett", new LatLng(location3.getLongitude(), location3.getLatitude()), "Service"));
+
+        Location location4 = getNearbyRandomLocation(
+                mLocation.getLongitude(), mLocation.getLatitude(), 5000);
+        mFeature.submitFlare(
+                new Flare("Sunny Raj", new LatLng(location4.getLongitude(), location4.getLatitude()), "Service"));
 
     }
 
