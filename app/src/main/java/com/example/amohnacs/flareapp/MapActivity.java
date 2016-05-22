f7df8b8d0528c4667ded64b54fe207d69aca7241
Sunny Raj <sundarcs@gwu.edu>
2016-05-22 08:47:14 -0400

    Rename flare to Flare

--- app/src/main/java/com/example/amohnacs/flareapp/MapActivity.java
+++ app/src/main/java/com/example/amohnacs/flareapp/MapActivity.java
@@ -108,7 +108,19 @@ public class MapActivity extends AppCompatActivity implements FeatureCallback{
             public void onSwipeTop() {
                 Toast.makeText(MapActivity.this, "Up", Toast.LENGTH_SHORT).show();
 
+                // create a point marker symbol (red, size 10, of type circle)
+                SimpleMarkerSymbol simpleMarker = new SimpleMarkerSymbol(Color.RED, 10, SimpleMarkerSymbol.STYLE.CIRCLE);
 
+                // create a point at x=-302557, y=7570663 (for a map using meters as units; this depends
+                // on the spatial reference)
+                //Point pointGeometry = new Point(mLocation.getLatitude(), mLocation.getLongitude());
+                Point pointGeometry = new Point(-302557, 7570663);
+
+                // create a graphic with the geometry and marker symbol
+                Graphic pointGraphic = new Graphic(pointGeometry, simpleMarker);
+
+                // add the graphic to the graphics layer
+                graphicsLayer.addGraphic(pointGraphic);
             }
 
             public void onSwipeRight() {
